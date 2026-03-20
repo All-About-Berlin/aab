4023dfbc19b82cce8217cda21b331ca18c60889f
Nicolas Bouliane <contact@nicolasbouliane.com>
2026-03-20 11:33:58 +0100

    ursus_config: Simplify

--- frontend/ursus_config.py
+++ frontend/ursus_config.py
@@ -37,99 +37,20 @@ def _load_constants(path):
     return result
 
 
-_v = _load_constants(Path(__file__).parent / "constants.json")
+ctx.update(_load_constants(Path(__file__).parent / "constants.json"))
 
 # ==============================================================================
-# TAXES
+# TAXES - Calculated values based on other constants
 # ==============================================================================
 
-# German minimum wage (€/h)
-ctx["MINIMUM_WAGE"] = _v["MINIMUM_WAGE"]
-
-ctx["MEDIAN_INCOME_BERLIN"] = _v["MEDIAN_INCOME_BERLIN"]
-ctx["MEDIAN_INCOME_GERMANY"] = _v["MEDIAN_INCOME_GERMANY"]
-
-ctx["AU_PAIR_MIN_ALLOWANCE"] = _v["AU_PAIR_MIN_ALLOWANCE"]
-
-# Maximum income used to calculate pension contributions (€/y)
-ctx["BEITRAGSBEMESSUNGSGRENZE"] = _v["BEITRAGSBEMESSUNGSGRENZE_MONTHLY"] * 12  # § SGB 6 Anlage 2 [BBGRV]
-
-ctx["GRUNDFREIBETRAG"] = _v["GRUNDFREIBETRAG"]  # § 32a EstG [GFB]
-ctx["INCOME_TAX_BRACKET_2_MAX_INCOME"] = _v["INCOME_TAX_BRACKET_2_MAX_INCOME"]  # § 32a EstG [UPTAB26 - 1]
-ctx["INCOME_TAX_BRACKET_3_MAX_INCOME"] = _v["INCOME_TAX_BRACKET_3_MAX_INCOME"]  # § 32a EstG [UPTAB26 - 1]
-ctx["INCOME_TAX_BRACKET_4_MAX_INCOME"] = _v["INCOME_TAX_BRACKET_4_MAX_INCOME"]  # § 32a EstG [UPTAB26 - 1]
-
-# Upper bound (€/y) of income tax tariff zones for tax classes 5 and 6
-ctx["INCOME_TAX_CLASS_56_LIMIT_1"] = _v["INCOME_TAX_CLASS_56_LIMIT_1"]  # § 39b Abs. 2 Satz 7 EstG [W1STKL5]
-ctx["INCOME_TAX_CLASS_56_LIMIT_2"] = _v["INCOME_TAX_CLASS_56_LIMIT_2"]  # § 39b Abs. 2 Satz 7 EstG [W2STKL5]
-ctx["INCOME_TAX_CLASS_56_LIMIT_3"] = _v["INCOME_TAX_CLASS_56_LIMIT_3"]  # § 39b Abs. 2 Satz 7 EstG [W3STKL5]
-
-ctx["INCOME_TAX_MAX_RATE"] = _v["INCOME_TAX_MAX_RATE"]  # (%) - § 32b EstG
-
-ctx["CHURCH_TAX_RATE"] = _v["CHURCH_TAX_RATE"]  # (%)
-ctx["CHURCH_TAX_RATE_BW_BY"] = _v["CHURCH_TAX_RATE_BW_BY"]  # (%)
-
-ctx["SOLIDARITY_TAX_MILDERUNGSZONE_MIN_INCOME_TAX"] = _v[
-    "SOLIDARITY_TAX_MILDERUNGSZONE_MIN_INCOME_TAX"
-]  # § 3 SolzG [SOLZFREI]
-ctx["SOLIDARITY_TAX_MILDERUNGSZONE_RATE"] = _v["SOLIDARITY_TAX_MILDERUNGSZONE_RATE"]  # § 4 SolzG
-ctx["SOLIDARITY_TAX_MAX_RATE"] = _v["SOLIDARITY_TAX_MAX_RATE"]  # § 4 SolzG
-
-ctx["WERBUNGSKOSTEN_PAUSCHALE"] = _v["WERBUNGSKOSTEN_PAUSCHALE"]
-ctx["VORSORGEPAUSCHAL_MIN"] = _v["VORSORGEPAUSCHAL_MIN"]  # § 39b Abs. 2.3.e EStG
-ctx["VORSORGEPAUSCHAL_MIN_TAX_CLASS_3"] = _v["VORSORGEPAUSCHAL_MIN_TAX_CLASS_3"]
-ctx["ARBEITNEHMERPAUSCHALE"] = _v["ARBEITNEHMERPAUSCHALE"]  # (€/y) - § 9a EStG
-ctx["SONDERAUSGABEN_PAUSCHBETRAG"] = _v["SONDERAUSGABEN_PAUSCHBETRAG"]  # (€/y) § 10c EStG [SAP]
-
-ctx["ARBEITSLOSENVERSICHERUNG_EMPLOYEE_RATE"] = _v["ARBEITSLOSENVERSICHERUNG_RATE"] / 2  # § 341 SGB 3, BeiSaV 2019
-
-# Kindergeld amount per child (€/m) - § 6 Abs. 1 BKGG, § 66 EStG
-ctx["KINDERGELD"] = _v["KINDERGELD"]
-
-# Tax break for parents (€/y) - § 32 Abs. 6 EStG [KFB] - monitored
-ctx["KINDERFREIBETRAG"] = _v["KINDERFREIBETRAG"]
-
-# Tax break for single parents (€/y) - § 24b EStG [EFA]
-ctx["ENTLASTUNGSBETRAG_ALLEINERZIEHENDE"] = _v["ENTLASTUNGSBETRAG_ALLEINERZIEHENDE"]
-ctx["ENTLASTUNGSBETRAG_ALLEINERZIEHENDE_EXTRA_CHILD"] = _v["ENTLASTUNGSBETRAG_ALLEINERZIEHENDE_EXTRA_CHILD"]
-
-ctx["CAPITAL_GAINS_TAX_RATE"] = _v["CAPITAL_GAINS_TAX_RATE"]  # (%) - § 32d EStG
-ctx["CAPITAL_GAINS_FREIBETRAG"] = _v["CAPITAL_GAINS_FREIBETRAG"]  # Sparer-Pauschbetrag, § 20 Abs. 9 EStG
-
-# Below that amount (€/y), you don't pay Gewerbesteuer - § 11 GewStG
-ctx["GEWERBESTEUER_FREIBETRAG"] = _v["GEWERBESTEUER_FREIBETRAG"]
-
-# Used as the basis, multiplied by the Hebesatz - (%) - § 11 GewStG
-ctx["GEWERBESTEUER_RATE"] = _v["GEWERBESTEUER_RATE"]
-
-# The part of the Gewerbesteuer that is credited from your income tax (%)
-ctx["GEWERBESTEUER_TAX_CREDIT"] = _v["GEWERBESTEUER_TAX_CREDIT"]  # (%) - TODO: Not watched, no source
-
-ctx["GEWERBESTEUER_HEBESATZ_BERLIN"] = _v["GEWERBESTEUER_HEBESATZ_BERLIN"]  # (%) - TODO: Not watched
-ctx["GEWERBESTEUER_RATE_BERLIN"] = (ctx["GEWERBESTEUER_RATE"] * ctx["GEWERBESTEUER_HEBESATZ_BERLIN"]).normalize()  # (%)
+ctx["BEITRAGSBEMESSUNGSGRENZE"] = ctx["BEITRAGSBEMESSUNGSGRENZE_MONTHLY"] * 12
+ctx["ARBEITSLOSENVERSICHERUNG_EMPLOYEE_RATE"] = ctx["ARBEITSLOSENVERSICHERUNG_RATE"] / 2  # § 341 SGB 3, BeiSaV 2019
+ctx["GEWERBESTEUER_RATE_BERLIN"] = (ctx["GEWERBESTEUER_RATE"] * ctx["GEWERBESTEUER_HEBESATZ_BERLIN"]).normalize()
 
 # The effective cost of the Gewerbesteuer when accounting for the income tax credit, for Berlin - (%)
 ctx["GEWERBESTEUER_EXTRA_COST_BERLIN"] = (
     ctx["GEWERBESTEUER_RATE"] * (ctx["GEWERBESTEUER_HEBESATZ_BERLIN"] - ctx["GEWERBESTEUER_TAX_CREDIT"])
 ).normalize()
-
-ctx["KLEINUNTERNEHMER_MAX_INCOME_FIRST_YEAR"] = _v["KLEINUNTERNEHMER_MAX_INCOME_FIRST_YEAR"]  # § 19 Abs. 1 UStG
-ctx["KLEINUNTERNEHMER_MAX_INCOME"] = _v["KLEINUNTERNEHMER_MAX_INCOME"]  # § 19 Abs. 1 UStG
-
-# Above that amount (€/y), you must use double entry bookkeeping - § 241a HGB
-ctx["DOUBLE_ENTRY_MIN_REVENUE"] = _v["DOUBLE_ENTRY_MIN_REVENUE"]
-ctx["DOUBLE_ENTRY_MIN_INCOME"] = _v["DOUBLE_ENTRY_MIN_INCOME"]
-
-# VAT (%) - § 12 UStG (Abs 1 and 2)
-ctx["VAT_RATE"] = _v["VAT_RATE"]
-ctx["VAT_RATE_REDUCED"] = _v["VAT_RATE_REDUCED"]
-
-# Below 10,000€/y in VAT, simplified rules for intra-EU VAT
-ctx["EU_VAT_SCHWELLENWERT"] = _v["EU_VAT_SCHWELLENWERT"]
-
-# Umsatzsteuer-Voranmeldung minimum amounts, based on VAT paid last year (€/year) - § 18 UStG
-ctx["VAT_MIN_QUARTERLY_AMOUNT"] = _v["VAT_MIN_QUARTERLY_AMOUNT"]
-ctx["VAT_MIN_MONTHLY_AMOUNT"] = _v["VAT_MIN_MONTHLY_AMOUNT"]
 
 
 # ==============================================================================
@@ -139,85 +60,44 @@ ctx["VAT_MIN_MONTHLY_AMOUNT"] = _v["VAT_MIN_MONTHLY_AMOUNT"]
 # Below this income (€/mth), you have a minijob
 ctx["MINIJOB_MAX_INCOME"] = round(ctx["MINIMUM_WAGE"] * 130 / 3)  # § 8 SGB IV
 
-# Below this income (€/mth), you have a midijob - § 20 SGB IV
-ctx["MIDIJOB_MAX_INCOME"] = _v["MIDIJOB_MAX_INCOME"]
-
-# Used to calculate health insurance for a midijob
-ctx["GKV_FACTOR_F"] = _v["GKV_FACTOR_F"]  # § 20 SGB IV - TODO: Can be calculated from other vals
-
-# Median income (€/m) of all people who pay social contribs
-ctx["BEZUGSGROESSE"] = _v["BEZUGSGROESSE"]  # SGB VI Anlage 1
-
 # Base contribution (%), including Krankengeld
-ctx["GKV_BASE_RATE_EMPLOYEE"] = _v["GKV_BASE_RATE_EMPLOYEE"]  # § 241 SGB V
 ctx["GKV_BASE_RATE_STUDENT"] = ctx["GKV_BASE_RATE_EMPLOYEE"] * Decimal("0.7")  # § 245 SGB V
-
-# Base contribution (%), excluding Krankengeld (freelanccers, unemployed, students over 30)
-ctx["GKV_BASE_RATE_SELF_PAY"] = _v["GKV_BASE_RATE_SELF_PAY"]  # § 243 SGB V
 
 # Mindestbemessungsgrundlage (€/mth) - Below this income, GKV does not get cheaper
 ctx["GKV_MIN_INCOME"] = ctx["BEZUGSGROESSE"] / 90 * 30  # § 240 Abs. 4 SGV IV
 
 # Above this income (€/y), you pay the Höchstbeitrag - https://www.bmas.de/DE/Arbeit/Arbeitsrecht/Mindestlohn/mindestlohn.html
-ctx["GKV_MAX_INCOME"] = _v["GKV_MAX_INCOME_MONTHLY"] * 12  # SVBezGrV 2021 [BBGKVPV]
-
-# Above this income (€/mth), your employer pays for health insurance
-ctx["GKV_AZUBI_FREIBETRAG"] = _v["GKV_AZUBI_FREIBETRAG"]  # § 20 Abs. 3 SGB IV
+ctx["GKV_MAX_INCOME"] = ctx["GKV_MAX_INCOME_MONTHLY"] * 12  # SVBezGrV 2021 [BBGKVPV]
 
 # Above this income, it's no longer a Nebenjob
 ctx["GKV_NEBENJOB_MAX_INCOME"] = ctx["BEZUGSGROESSE"] * Decimal("0.75")
 
 # Jahresarbeitsentgeltgrenze or Versicherungspflichtgrenze - Above this income (€/y), you are freiwillig versichert
-ctx["GKV_FREIWILLIG_VERSICHERT_MIN_INCOME"] = _v["GKV_FREIWILLIG_VERSICHERT_MIN_INCOME_MONTHLY"] * 12
-
-# If you earn less than that (€/y), private health insurers usually reject you
-ctx["PKV_MIN_INCOME"] = _v["PKV_MIN_INCOME"]
+ctx["GKV_FREIWILLIG_VERSICHERT_MIN_INCOME"] = ctx["GKV_FREIWILLIG_VERSICHERT_MIN_INCOME_MONTHLY"] * 12
 
 # Above this income (€/m), you can't have Familienversicherung
 ctx["GKV_FAMILIENVERSICHERUNG_MAX_INCOME"] = (Decimal(1 / 7) * ctx["BEZUGSGROESSE"]).normalize()  # § 10 SGB V
 
-# Zusatzbeiträge - https://www.check24.de/gesetzliche-krankenversicherung/erhoehung-zusatzbeitraege/
-ctx["GKV_MIN_ZUSATZBEITRAG"] = _v["GKV_MIN_ZUSATZBEITRAG"]  # HKK
-ctx["GKV_MAX_ZUSATZBEITRAG"] = _v["GKV_MAX_ZUSATZBEITRAG"]  # AOK Nordost
-ctx["GKV_AVG_ZUSATZBEITRAG"] = _v["GKV_AVG_ZUSATZBEITRAG"]
-
 ctx["GKV_ZUSATZBEITRAG_AVERAGE"] = ctx["GKV_AVG_ZUSATZBEITRAG"]
-ctx["GKV_ZUSATZBEITRAG_AOK"] = _v["GKV_ZUSATZBEITRAG_AOK"]
-ctx["GKV_ZUSATZBEITRAG_BARMER"] = _v["GKV_ZUSATZBEITRAG_BARMER"]
-ctx["GKV_ZUSATZBEITRAG_DAK"] = _v["GKV_ZUSATZBEITRAG_DAK"]
-ctx["GKV_ZUSATZBEITRAG_HKK"] = _v["GKV_ZUSATZBEITRAG_HKK"]
-ctx["GKV_ZUSATZBEITRAG_TK"] = _v["GKV_ZUSATZBEITRAG_TK"]
 
-ctx["TRAVEL_INSURANCE_COST"] = _v["TRAVEL_INSURANCE_COST"]
 ctx["EXPAT_INSURANCE_COST"] = {
-    "feather-basic": _v["EXPAT_INSURANCE_COST_FEATHER_BASIC"],
-    "feather-premium": _v["EXPAT_INSURANCE_COST_FEATHER_PREMIUM"],
-    "ottonova-expat": _v["EXPAT_INSURANCE_COST_OTTONOVA_EXPAT"],
-    "hansemerkur-basic": _v["EXPAT_INSURANCE_COST_HANSEMERKUR_BASIC"],
-    "hansemerkur-profi": _v["EXPAT_INSURANCE_COST_HANSEMERKUR_PROFI"],
+    "feather-basic": ctx["EXPAT_INSURANCE_COST_FEATHER_BASIC"],
+    "feather-premium": ctx["EXPAT_INSURANCE_COST_FEATHER_PREMIUM"],
+    "ottonova-expat": ctx["EXPAT_INSURANCE_COST_OTTONOVA_EXPAT"],
+    "hansemerkur-basic": ctx["EXPAT_INSURANCE_COST_HANSEMERKUR_BASIC"],
+    "hansemerkur-profi": ctx["EXPAT_INSURANCE_COST_HANSEMERKUR_PROFI"],
 }
-
-ctx["EXPAT_STUDENT_COST"] = _v["EXPAT_STUDENT_COST"]  # /out/feather-expats
 
 # Maximum daily Krankengeld
 ctx["GKV_KRANKENGELD_DAILY_LIMIT"] = (ctx["GKV_MAX_INCOME"] * Decimal("0.7") / 360).normalize()  # § 47 SGB V
 
 # BAFöG Bedarfssatz (€/y)
-ctx["BAFOG_BEDARFSSATZ"] = _v["BAFOG_BEDARFSSATZ"]  # § 13 BAföG Abs 1.2 + 2.2
 ctx["SPERRKONTO_AMOUNT"] = (
-    ctx["BAFOG_BEDARFSSATZ"] + _v["SPERRKONTO_SURCHARGE_INSURANCE"] + _v["SPERRKONTO_SURCHARGE_OTHER"]
+    ctx["BAFOG_BEDARFSSATZ"] + ctx["SPERRKONTO_SURCHARGE_INSURANCE"] + ctx["SPERRKONTO_SURCHARGE_OTHER"]
 ) * 12  # § 13 BAföG Abs 1.2 + 2.2 + § 13a BAföG Abs 1
 
 # Pflegeversicherung (%) - § 55 Abs. 1 SGB XI, can be changed in external regulation (like PBAV 2026)
-ctx["PFLEGEVERSICHERUNG_BASE_RATE"] = _v["PFLEGEVERSICHERUNG_BASE_RATE"]
-ctx["PFLEGEVERSICHERUNG_BASE_RATE_MAX_AGE"] = _v["PFLEGEVERSICHERUNG_BASE_RATE_MAX_AGE"]  # § 55 Abs. 1 SGB XI
 ctx["PFLEGEVERSICHERUNG_EMPLOYER_RATE"] = ctx["PFLEGEVERSICHERUNG_BASE_RATE"] / 2
-
-# Surcharge for people over 23 with no kids
-ctx["PFLEGEVERSICHERUNGS_SURCHARGE"] = _v["PFLEGEVERSICHERUNGS_SURCHARGE"]  # § 55 Abs. 3 SGB XI
-ctx["PFLEGEVERSICHERUNG_DISCOUNT_PER_CHILD"] = _v["PFLEGEVERSICHERUNG_DISCOUNT_PER_CHILD"]  # § 55 Abs. 3 SGB XI
-ctx["PFLEGEVERSICHERUNG_DISCOUNT_MIN_CHILDREN"] = _v["PFLEGEVERSICHERUNG_DISCOUNT_MIN_CHILDREN"]
-ctx["PFLEGEVERSICHERUNG_DISCOUNT_MAX_CHILDREN"] = _v["PFLEGEVERSICHERUNG_DISCOUNT_MAX_CHILDREN"]
 
 ctx["PFLEGEVERSICHERUNG_MIN_RATE"] = (
     ctx["PFLEGEVERSICHERUNG_BASE_RATE"]
@@ -232,17 +112,7 @@ ctx["PFLEGEVERSICHERUNG_MAX_RATE"] = (
 # ==============================================================================
 
 # Public pension contribution (%) - RVBeitrSBek 202X
-ctx["RV_BASE_RATE"] = _v["RV_BASE_RATE"]  # RVBeitrSBek 202X
-ctx["RV_EMPLOYEE_CONTRIBUTION"] = _v["RV_EMPLOYEE_CONTRIBUTION"]
 ctx["RV_MIN_CONTRIBUTION"] = (ctx["RV_BASE_RATE"] * ctx["MINIJOB_MAX_INCOME"] / 100).normalize()
-
-ctx["FUNDSBACK_FEE"] = _v["FUNDSBACK_FEE"]  # %
-ctx["FUNDSBACK_MIN_FEE"] = _v["FUNDSBACK_MIN_FEE"]  # €
-ctx["FUNDSBACK_MAX_FEE"] = _v["FUNDSBACK_MAX_FEE"]  # €
-ctx["GERMANYPENSIONREFUND_FEE"] = _v["GERMANYPENSIONREFUND_FEE"]  # %
-ctx["PENSIONREFUNDGERMANY_FEE"] = _v["PENSIONREFUNDGERMANY_FEE"]  # %
-ctx["PENSIONREFUNDGERMANY_MAX_FEE"] = _v["PENSIONREFUNDGERMANY_MAX_FEE"]  # €
-ctx["GERMANYPENSIONREFUND_MAX_FEE"] = _v["GERMANYPENSIONREFUND_MAX_FEE"]  # €
 
 
 gkv_min_rate_employee = (  # Total rate for employees
@@ -299,17 +169,6 @@ ctx["GKV_COST_STUDENT"] = round(
 
 
 # ==============================================================================
-# PUBLIC TRANSIT
-# ==============================================================================
-
-ctx["BVG_AB_TICKET"] = _v["BVG_AB_TICKET"]
-ctx["BVG_ABC_TICKET"] = _v["BVG_ABC_TICKET"]
-ctx["BVG_FINE"] = _v["BVG_FINE"]
-ctx["BVG_REDUCED_FINE"] = _v["BVG_REDUCED_FINE"]
-ctx["DEUTSCHLAND_TICKET_PRICE"] = _v["DEUTSCHLAND_TICKET_PRICE"]
-
-
-# ==============================================================================
 # IMMIGRATION
 # ==============================================================================
 
@@ -319,29 +178,12 @@ ctx["BLUE_CARD_MIN_INCOME"] = round(Decimal("0.5") * ctx["BEITRAGSBEMESSUNGSGREN
 # Minimum income (€/y) to get a Blue Card in shortage fields - § 18g AufenthG
 ctx["BLUE_CARD_SHORTAGE_MIN_INCOME"] = round(Decimal("0.453") * ctx["BEITRAGSBEMESSUNGSGRENZE"])
 
-
-# Visa fees (€) - § 44, § 45, § 45c and § 47 AufenthV
-ctx["SCHENGEN_VISA_FEE"] = _v["SCHENGEN_VISA_FEE"]
-ctx["NATIONAL_VISA_FEE"] = _v["NATIONAL_VISA_FEE"]
-ctx["NATIONAL_VISA_RENEWAL_FEE"] = _v["NATIONAL_VISA_RENEWAL_FEE"]
-ctx["RESIDENCE_PERMIT_REPLACEMENT_FEE"] = _v["RESIDENCE_PERMIT_REPLACEMENT_FEE"]  # § 45c AufenthG
-ctx["MIN_PERMANENT_RESIDENCE_FEE"] = _v["MIN_PERMANENT_RESIDENCE_FEE"]  # For Turkish citizens
-ctx["MAX_PERMANENT_RESIDENCE_FEE"] = _v["MAX_PERMANENT_RESIDENCE_FEE"]  # § 44 AufenthG
-ctx["FAST_TRACK_FEE"] = _v["FAST_TRACK_FEE"]  # § 47 AufenthG
-
 # Minimum guaranteed pension payment (€/m) to get a freelance visa above age 45
 # VAB, https://www.bmas.de/DE/Soziales/Rente-und-Altersvorsorge/rentenversicherungsbericht-art.html
-ctx["FREELANCE_VISA_MIN_MONTHLY_PENSION"] = _v["FREELANCE_VISA_MIN_MONTHLY_PENSION"]
 ctx["FREELANCE_VISA_MIN_PENSION"] = round(ctx["FREELANCE_VISA_MIN_MONTHLY_PENSION"] * 144)
-
-# Minimum income (€/mth) before health insurance and rent to get a freelance visa - Anlage SGB 12 (Regelbedarfsstufe 1)
-ctx["FREELANCE_VISA_MIN_INCOME"] = _v["FREELANCE_VISA_MIN_INCOME"]
 
 # Minimum gross income (€/y) to get a work visa above age 45 - service.berlin.de/dienstleistung/305304
 ctx["WORK_VISA_MIN_INCOME"] = ctx["BEITRAGSBEMESSUNGSGRENZE"] * Decimal("0.55")
-
-# Not watched - https://www.berlin.de/vhs-tempelhof-schoeneberg/kurse/deutsch-als-zweitsprache/pruefungen-und-abschluesse/einbuergerung/
-ctx["CITIZENSHIP_TEST_FEE"] = _v["CITIZENSHIP_TEST_FEE"]
 
 # Nationalities that can apply for a residence permit directly in Germany - § 41 AufenthV
 beschv_26_1_countries = [
@@ -400,40 +242,8 @@ ctx["AUFENTHV_41_COUNTRIES"] = or_join(
 # ADMINISTRATION
 # ==============================================================================
 
-ctx["BESCHEINIGUNG_IN_STEUERSACHEN_FEE"] = _v["BESCHEINIGUNG_IN_STEUERSACHEN_FEE"]  # dienstleistung/324713
-ctx["ERWEITERTE_MELDEBESCHEINIGUNG_FEE"] = _v[
-    "ERWEITERTE_MELDEBESCHEINIGUNG_FEE"
-]  # (€) - service.berlin.de/dienstleistung/120702
-ctx["GEWERBEANMELDUNG_FEE"] = _v["GEWERBEANMELDUNG_FEE"]  # € - service.berlin.de/dienstleistung/121921
-ctx["HUNDEREGISTER_FEE"] = _v["HUNDEREGISTER_FEE"]  # € - hunderegister.berlin.de
-ctx["HUNDESTEUER_FIRST_DOG"] = _v["HUNDESTEUER_FIRST_DOG"]  # §4 HuStG BE, (€/y)
-ctx["HUNDESTEUER_MORE_DOGS"] = _v["HUNDESTEUER_MORE_DOGS"]  # §4 HuStG BE, (€/y)
-
 # Maximum income from employment to stay a member of the KSK (€/y)
 ctx["KSK_MAX_EMPLOYMENT_INCOME"] = ctx["BEITRAGSBEMESSUNGSGRENZE"] / 2  # § 4 KSVG
-ctx["KSK_MIN_INCOME"] = _v["KSK_MIN_INCOME"]  # (€/y) - §3 Abs. 1 KSVG
-
-# Minimum income used to calculate cost of health insurance and Pflegeversicherung
-ctx["KSK_MIN_HEALTH_INSURANCE_INCOME"] = _v[
-    "KSK_MIN_HEALTH_INSURANCE_INCOME"
-]  # Mindestbeitragsberechnungsgrundlage (€/y)
-
-ctx["ORDNUNGSAMT_DANGEROUS_DOG_FEE"] = _v["ORDNUNGSAMT_DANGEROUS_DOG_FEE"]  # service.berlin.de/dienstleistung/326263
-ctx["RUNDFUNKBEITRAG_FEE"] = _v["RUNDFUNKBEITRAG_FEE"]
-ctx["SCHUFA_REPORT_FEE"] = _v["SCHUFA_REPORT_FEE"]  # TODO: Not watched
-ctx["VEHICLE_UMMELDUNG_FEE"] = _v["VEHICLE_UMMELDUNG_FEE"]  # service.berlin.de/dienstleistung/120658
-ctx["LICENSE_PLATE_COST"] = _v["LICENSE_PLATE_COST"]
-
-ctx["FIRST_AID_COURSE_COST"] = _v["FIRST_AID_COURSE_COST"]
-ctx["DRIVING_LICENCE_CONVERSION_FEE"] = _v["DRIVING_LICENCE_CONVERSION_FEE"]  # (€) - /dienstleistung/327537
-ctx["DRIVING_LICENCE_FEE"] = _v["DRIVING_LICENCE_FEE"]  # (€) - service.berlin.de/dienstleistung/121627
-ctx["FIRST_AID_COURSE_FEE"] = _v["FIRST_AID_COURSE_FEE"]
-ctx["DRIVING_SCHOOL_FEE"] = _v["DRIVING_SCHOOL_FEE"]
-ctx["DRIVING_PRACTICE_FEE"] = _v["DRIVING_PRACTICE_FEE"]  # per 45-minute lesson
-ctx["DRIVING_THEORY_EXAM_FEE"] = _v["DRIVING_THEORY_EXAM_FEE"]  # Dekra/TÜV fee
-ctx["DRIVING_PRACTICAL_EXAM_FEE"] = _v["DRIVING_PRACTICAL_EXAM_FEE"]  # Dekra/TÜV fee
-
-ctx["LEGAL_HOTLINE_COST_PER_MINUTE"] = _v["LEGAL_HOTLINE_COST_PER_MINUTE"]
 
 # ==============================================================================
 # DATES
