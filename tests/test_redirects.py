be59482ff7ceb0e9a92d1f4a8eed6d3a3deebc23
Nicolas Bouliane <contact@nicolasbouliane.com>
2026-05-13 15:14:58 +0200

    services: Fall back to All About Berlin resources if they don't exist on A Good Start. Load Plausible and exchange rates properly.

--- tests/test_redirects.py
+++ tests/test_redirects.py
@@ -4,6 +4,7 @@ import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+
 @pytest.mark.parametrize(
     "path, expected_location",
     [
@@ -17,3 +18,16 @@ def test_services_redirect(path, expected_location, base_url):
     response = requests.get(base_url.replace("http://", "https://") + path, allow_redirects=False, verify=False)
     assert response.status_code == 301
     assert response.headers["Location"] == expected_location
+
+
+@pytest.mark.parametrize(
+    "path",
+    [
+        "/health-insurance",
+        "/dental-insurance",
+        "/about",
+    ],
+)
+def test_services_pages(path):
+    response = requests.get("https://services.localhost" + path, verify=False)
+    assert response.status_code == 200
