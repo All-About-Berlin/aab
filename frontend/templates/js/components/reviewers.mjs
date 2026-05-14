5ec44989db324c1262f2209513c9b0b929be1ec8
Nicolas Bouliane <contact@nicolasbouliane.com>
2026-05-14 12:37:13 +0200

    guides: More accessible reviewer list

--- frontend/templates/js/components/reviewers.mjs
+++ frontend/templates/js/components/reviewers.mjs
@@ -1,7 +1,6 @@
 export default function initializeReviewers(){
 	window.addEventListener("DOMContentLoaded", function() {
-		document.querySelectorAll('.post-reviewers a').forEach(link => link.addEventListener('click', e => {
-			e.preventDefault();
+		document.querySelectorAll('.post-reviewers button').forEach(link => link.addEventListener('click', e => {
 			link.classList.toggle('expanded');
 			document.getElementById('reviewers').classList.toggle('hidden');
 		}));
