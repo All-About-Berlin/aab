47957470cc45133a555791b24b2befc08d7cb344
Nicolas Bouliane <contact@nicolasbouliane.com>
2026-06-10 23:04:12 +0200

    glossary-tooltip: Generate the HTML on the fly, so it's not in the DOM by default

--- frontend/templates/js/components/glossary-tooltip.mjs
+++ frontend/templates/js/components/glossary-tooltip.mjs
@@ -1,39 +1,74 @@
 import { getNearestHeadingId } from '/js/utils/tracking.mjs';
 
-const tooltip = document.getElementById('glossary-tooltip');
+let dialog = null; // The tooltip <dialog> element
 let pronounciationAudio = null;
 
-export function showTooltip(clickEvent) {
-	const tooltipBody = tooltip.querySelector('.article-body');
+function createTooltip() {
+	if (dialog) return;
 
-	// When switching from a tooltip to another, prevent the old content from showing on the new tooltip
-	tooltip.querySelector('h2 a dfn').innerHTML = 'Loading…';
-	tooltip.querySelector('h2 a small').innerHTML = '…';
-	tooltipBody.innerHTML = '<p>...</p>';
+	dialog = document.createElement('dialog');
+	dialog.id = 'glossary-tooltip';
+	dialog.setAttribute('aria-labelledby', 'h-dialog');
+	dialog.setAttribute('closedby', 'any');
+	dialog.innerHTML = `<header>
+		<h2>
+			<a href="#" title="Open definition in new tab" target="_blank">
+				<dfn role="term" lang="en" id="h-dialog"></dfn>
+				<small lang="de"></small>
+			</a>
+		</h2>
+		<button title="Hear how this term is pronounced" class="pronounce-button" role="button" aria-label="Hear how this term is pronounced"><i class="icon sound" aria-hidden="true"></i></button>
+		<button title="Close" class="close-button" role="button" aria-label="Close dialog"><i class="icon close" aria-hidden="true"></i></button>
+	</header>
+	<div class="article-body" role="definition"></div>`;
+	document.body.appendChild(dialog);
+
+	dialog.querySelector('.close-button').addEventListener('click', hideTooltip);
+	dialog.querySelector('.pronounce-button').addEventListener('click', pronounceTerm);
+
+	dialog = dialog;
+}
+
+function setTooltipAsLoading(dialog){
+	dialog.querySelector('h2 a dfn').innerHTML = 'Loading…';
+	dialog.querySelector('h2 a small').innerHTML = '…';
+	dialog.querySelector('.article-body').innerHTML = '<p>…</p>';
+}
+
+export function showTooltip(clickEvent) {
+	createTooltip();
+	setTooltipAsLoading(dialog);
+
+	// Pause previous audio
+	if (pronounciationAudio) {
+		pronounciationAudio.pause();
+	}
 
 	// Fetch description
 	const anchor = clickEvent.currentTarget || clickEvent.target;
-	if(!tooltip.open){
-		tooltip.showModal();
+	if (!dialog.open) {
+		dialog.showModal();
 	}
 	fetch(anchor.getAttribute('href') + '.json').then(r => r.json()).then(data => {
-		tooltip.querySelector('h2 a').setAttribute('href', anchor.getAttribute('href'));
-		tooltip.querySelector('h2 a dfn').innerHTML = data.englishTerm || data.germanTerm;
-		tooltip.querySelector('h2 a small').innerHTML = data.germanTerm || '';
-		tooltip.querySelector('h2 a small').classList.toggle('hidden', (!data.englishTerm || data.englishTerm == data.germanTerm));
-		tooltipBody.innerHTML = data.definition;
-		tooltipBody.querySelectorAll('a').forEach(a => a.target = '_blank');
+		dialog.querySelector('h2 a').setAttribute('href', anchor.getAttribute('href'));
+		dialog.querySelector('h2 a dfn').innerHTML = data.englishTerm || data.germanTerm;
+		dialog.querySelector('h2 a small').innerHTML = data.germanTerm || '';
+		dialog.querySelector('h2 a small').classList.toggle('hidden', (!data.englishTerm || data.englishTerm == data.germanTerm));
+
+		const dialogBody = dialog.querySelector('.article-body');
+		dialogBody.innerHTML = data.definition;
+		dialogBody.querySelectorAll('a').forEach(a => a.target = '_blank');
 
 		pronounciationAudio = new Audio(data.audioUrl);
-		tooltip.querySelector('.pronounce-button').href = data.audioUrl;
+		dialog.querySelector('.pronounce-button').href = data.audioUrl;
 
-		const footnotes = tooltipBody.querySelector('#footnotes');
-		if(footnotes){
+		const footnotes = dialogBody.querySelector('#footnotes');
+		if (footnotes) {
 			footnotes.remove();
 		}
-		setTooltipLinks(tooltipBody);
+		setDialogLinks(dialogBody);
 
-		plausible('Glossary tooltip', { props: {
+		plausible('Glossary dialog', { props: {
 			url: anchor.getAttribute('href'),
 			pageSection: getNearestHeadingId(anchor)
 		}});
@@ -42,45 +77,38 @@ export function showTooltip(clickEvent) {
 
 function hideTooltip(event) {
 	event.preventDefault();
-	tooltip.close();
-	if(pronounciationAudio){
+	dialog.close();
+	if (pronounciationAudio) {
 		pronounciationAudio.pause();
 	}
 }
 
-function setTooltipLinks(element){
-	element.querySelectorAll('a[href*="/glossary/"]').forEach((anchor) => {
-		if(typeof tooltip.show === 'function') {
-			anchor.addEventListener('click', (event) => {
+function setDialogLinks(element) {
+	element.querySelectorAll('a[href*="/glossary/"]').forEach(anchor => {
+		// If HTML5 dialogs are supported - otherwise default to link
+		if ('showModal' in document.createElement('dialog')) {
+			anchor.addEventListener('click', event => {
 				event.preventDefault();
 				event.stopPropagation();
 				showTooltip(event);
 			});
 
-			if(!anchor.classList.contains('recommended')){
-				anchor.classList.add('glossary-link')
+			if (!anchor.classList.contains('recommended')) {
+				anchor.classList.add('glossary-link');
 			}
-		}
-		else {
+		} else {
 			anchor.setAttribute('target', '_blank');
 		}
 	});
 }
 
-function pronounceTerm(event){
+function pronounceTerm(event) {
 	event.preventDefault();
 	pronounciationAudio.play();
 }
 
-export function initializeGlossaryTooltip(){
-	window.addEventListener("DOMContentLoaded", function() {
-		tooltip.querySelector('.close-button').addEventListener('click', hideTooltip);
-		tooltip.addEventListener('click', clickEvent => {
-			if(clickEvent.target === tooltip) {
-				tooltip.close();
-			}
-		});
-		document.querySelectorAll('.glossary-links').forEach(el => setTooltipLinks(el));
-		tooltip.querySelector('.pronounce-button').addEventListener('click', pronounceTerm);
+export default function initializeGlossaryTooltip() {
+	window.addEventListener('DOMContentLoaded', function() {
+		document.querySelectorAll('.glossary-links').forEach(el => setDialogLinks(el));
 	});
 }
