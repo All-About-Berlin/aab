69868fb543100ca5780654290fc2e02d28dc22d1
Nicolas Bouliane <contact@nicolasbouliane.com>
2026-08-07 16:04:06 +0200

    settings: Fix API test auth issues

--- backend/src/api/settings.py
+++ backend/src/api/settings.py
@@ -117,8 +117,8 @@ LOGGING = {
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
