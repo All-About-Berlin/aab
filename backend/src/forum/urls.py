f995fc637baddfca12ea2d2786394baca4cdfbcd
Nicolas Bouliane <contact@nicolasbouliane.com>
2026-08-16 11:53:52 +0200

    forum: Change rendering method, add user profile page

--- backend/src/forum/urls.py
+++ backend/src/forum/urls.py
@@ -6,6 +6,7 @@ from forum import views
 urlpatterns = [
     path("forum", views.forum_index, name="forum-index"),
     path("forum/page-<int:page>", views.forum_index, name="forum-index-page"),
+    path("forum/users/<str:username>", views.forum_user_profile, name="forum-user-profile"),
     path("forum/<int:thread_id>", views.forum_thread, name="forum-thread"),
     path("forum/<int:thread_id>/page-<int:page>", views.forum_thread, name="forum-thread-page"),
 ]
