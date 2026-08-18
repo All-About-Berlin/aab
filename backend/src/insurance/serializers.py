40e4df2d54fc5eeff9e8484e5a8f613de765c151
Nicolas Bouliane <contact@nicolasbouliane.com>
2026-08-18 13:55:07 +0200

    api/insurance: Create /signup endpoint

--- backend/src/insurance/serializers.py
+++ backend/src/insurance/serializers.py
@@ -1,6 +1,6 @@
 from django_countries.serializers import CountryFieldMixin
 from rest_framework.serializers import ModelSerializer
-from .models import Case
+from .models import Case, HealthInsuranceSignup
 
 
 class CaseSerializer(CountryFieldMixin, ModelSerializer):
@@ -8,3 +8,15 @@ class CaseSerializer(CountryFieldMixin, ModelSerializer):
         model = Case
         fields = "__all__"
         read_only_fields = ["site"]
+
+
+class HealthInsuranceSignupSerializer(CountryFieldMixin, ModelSerializer):
+    class Meta:
+        model = HealthInsuranceSignup
+        fields = "__all__"
+        read_only_fields = ["site", "status", "submission_date"]
+
+    def validate(self, attrs):
+        instance = HealthInsuranceSignup(**attrs)
+        instance.clean()
+        return attrs
--- backend/src/insurance/urls.py
+++ backend/src/insurance/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import CaseViewSet
+from .views import CaseViewSet, HealthInsuranceSignupViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r"case", CaseViewSet)
+router.register(r"signup", HealthInsuranceSignupViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
