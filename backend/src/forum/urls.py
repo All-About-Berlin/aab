08f3ca7d195f7e3f20c72bd02b107171165fbfb6
Nicolas Bouliane <contact@nicolasbouliane.com>
2026-09-07 17:40:23 +0200

    forum: Switch to class-based views

--- backend/src/forum/urls.py
+++ backend/src/forum/urls.py
@@ -5,10 +5,10 @@ from forum import views
 
 
 urlpatterns = [
-    path("forum", views.forum_index, name="forum_index"),
-    path("forum/page-<int:page>", views.forum_index, name="forum_index_page"),
-    path("forum/signup/welcome", views.forum_signup_welcome, name="forum_signup_welcome"),
-    path("forum/rules", views.forum_rules, name="forum_rules"),
+    path("forum", views.ForumIndexView.as_view(), name="forum_index"),
+    path("forum/page-<int:page>", views.ForumIndexView.as_view(), name="forum_index_page"),
+    path("forum/signup/welcome", views.ForumSignupWelcomeView.as_view(), name="forum_signup_welcome"),
+    path("forum/rules", views.ForumRulesView.as_view(), name="forum_rules"),
     path("forum/login", allauth_views.login, name="account_login"),
     path("forum/logout", allauth_views.logout, name="account_logout"),
     path("forum/signup", allauth_views.signup, name="account_signup"),
@@ -26,8 +26,8 @@ urlpatterns = [
         allauth_views.password_reset_from_key_done,
         name="account_reset_password_from_key_done",
     ),
-    path("forum/users/<str:username>", views.forum_user_profile, name="forum_user"),
-    path("forum/new", views.forum_new_thread, name="forum_new_thread"),
-    path("forum/<int:thread_id>", views.forum_thread, name="forum_thread"),
-    path("forum/<int:thread_id>/page-<int:page>", views.forum_thread, name="forum_thread_page"),
+    path("forum/users/<str:username>", views.ForumUserProfileView.as_view(), name="forum_user"),
+    path("forum/new", views.ForumNewThreadView.as_view(), name="forum_new_thread"),
+    path("forum/<int:thread_id>", views.ForumThreadView.as_view(), name="forum_thread"),
+    path("forum/<int:thread_id>/page-<int:page>", views.ForumThreadView.as_view(), name="forum_thread_page"),
 ]
