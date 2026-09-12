0acc5e3ce0f9a0ce91b3385a59739206f92a0741
Nicolas Bouliane <contact@nicolasbouliane.com>
2026-09-12 22:02:54 +0200

    forum: Simplify reply update signal

--- backend/src/forum/signals.py
+++ backend/src/forum/signals.py
@@ -1,10 +1,9 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.utils import timezone
 
-from forum.models import Reply, Thread
+from forum.models import Reply
 
 
 @receiver(post_save, sender=Reply)
 def bump_thread_modification_date(sender, instance, **kwargs):
-    Thread.objects.filter(pk=instance.thread_id).update(modification_date=timezone.now())
+    instance.thread.save()  # Trigger a change on the parent thread
